6f78ee788c33ef7fa8073d479a945be5bd9161c8
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-27 03:00:12 +0330

    feat(finish): finish code for version

--- conter.js
+++ conter.js
@@ -1,3 +1,14 @@
+const span = document.querySelector("span")
+let count = localStorage?.getItem("count") ?? 0
+    
+const make = () => {
+    span.innerText = count
+    localStorage.setItem("count", count)
+}
+
+make()
+
 document.querySelector("button").addEventListener("click", () => {
-    console.log("ok");
+    count++
+    make
 })
