75cf5552416200ec513108d49459e0ed11cfb705
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-27 03:03:37 +0330

    feat(finish): finish code for version 2

--- conter.js
+++ conter.js
@@ -1,8 +1,8 @@
 const span = document.querySelector("span")
-let count = localStorage?.getItem("count") ?? 0
+let count = parseInt(localStorage?.getItem("count")) ?? 0
     
 const make = () => {
-    span.innerText = count
+    span.innerHTML = count
     localStorage.setItem("count", count)
 }
 
