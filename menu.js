805fd9fd9f69a4ab89e85d9bfcc15df131244fb7
tikrack <tikrackcode@gmail.com>
2025-02-15 06:43:56 -0800

    fix(open): handle log After open or close menu

--- menu.js
+++ menu.js
@@ -1,14 +1,15 @@
 const $ = document
 const elm_menu_btn = $.querySelector("button.منو")
 const elm_menu = $.querySelector("div.منو")
-const elm_close_menu = $.querySelector("button.بستن")
 
 elm_menu_btn.addEventListener("click", () => {
     console.log(elm_menu.style.right);
     
     if (elm_menu.style.right === "0px") {
+        console.log("close")
         elm_menu.style.right === "-100%"
     }else {
+        console.log("open");
         elm_menu.style.right = "0"
     }
 })
