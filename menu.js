e8db680181d905a75e62c69c73ee5e4af9ef74b5
tikrack <tikrackcode@gmail.com>
2025-02-15 06:37:51 -0800

    chore(log)

--- menu.js
+++ menu.js
@@ -4,6 +4,8 @@ const elm_menu = $.querySelector("div.منو")
 const elm_close_menu = $.querySelector("button.بستن")
 
 elm_menu_btn.addEventListener("click", () => {
+    console.log(elm_menu.style.right);
+    
     if (elm_menu.style.right === "0") {
         elm_menu.style.right === "-100%"
     }else {
