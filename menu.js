059b7c52b6707bfb6baea7e9e4865ef2636aa778
tikrack <tikrackcode@gmail.com>
2025-02-28 23:46:38 +0330

    feat(show)

--- menu.js
+++ menu.js
@@ -1,17 +1,16 @@
 const $ = document
-const elm_menu_btn = $.querySelector("button.منو")
+const elm_menu_btn = $.querySelector("button.منو.باز")
+const elm_menu_btn_close = $.querySelector("button.منو.بسته")
 const elm_menu = $.querySelector("div.منو")
 
-elm_menu_btn.addEventListener("click", () => {
-    console.log(elm_menu.style.right);
-    
+elm_menu_btn.addEventListener("click", () => {    
     if (elm_menu.style.right === "0px") {
-        console.log("close")
-        console.log(elm_menu)
-        console.log(elm_menu.classList)
         elm_menu.style.right = "-100%"
     }else {
-        console.log("open");
         elm_menu.style.right = "0"
     }
+})
+
+elm_menu_btn_close.addEventListener("click", () => {    
+        elm_menu.style.right = "-100%"
 })
