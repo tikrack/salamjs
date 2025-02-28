34ee371d6ea8f2223ab43efc6c84eaddab084d72
tikrack <tikrackcode@gmail.com>
2025-02-28 23:35:51 +0330

    feat(show)

--- show.js
+++ show.js
@@ -4,11 +4,11 @@ let btn = $.querySelector("button.نمایشی")
 let div = $.querySelector("div.نمایشی")
 
 btn.addEventListener("click", () => {
-    console.log(div.style.display);
-    
-    if (div.style.display == "block" || div.style.display == "") {
-        div.style.display === "none"
+    div.style.display = "none"
+
+    if (div.style.display === "none") {
+        div.style.display = "block"
     }else {
-        div.style.display === "block"
+        div.style.display = "none"
     }
 })
